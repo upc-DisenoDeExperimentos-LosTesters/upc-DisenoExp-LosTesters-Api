0fce4002b316e6cff434cbdb7a9f99df4cc76526
JDu202012207 <u202012207@upc.edu.pe>
2025-04-26 17:23:59 -0500

    fix(program): remove ensure-created to prevent crash on production rds

--- BicasTeam.MoviGestion.API/Program.cs
+++ BicasTeam.MoviGestion.API/Program.cs
@@ -27,6 +27,7 @@ using BicasTeam.MoviGestion.API.Shipments.Application.Internal.QueryServices;
 using BicasTeam.MoviGestion.API.Shipments.Domain.Repositories;
 using BicasTeam.MoviGestion.API.Shipments.Domain.Services;
 using BicasTeam.MoviGestion.API.Shipments.Infrastructure.Persistence.EFC.Repositories;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -35,19 +36,14 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 // Add Configuration for Routing
-
 builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
 
 // Configure Lowercase URLs
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-// Add Database Connection
+// Database Connection
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Configure Database Context and Logging Levels
-
 
 if (builder.Environment.IsDevelopment())
 {
@@ -62,22 +58,20 @@ else
         options.UseMySQL(connectionString));
 }
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    c =>
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
-        c.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title   = "LosTesters.MoviGestion.API",
-            Version = "v1",
-            Description = "Los Testers MoviGestion Platform API",
-            TermsOfService = new Uri("https://testers-movigestion.com/tos"),
-            Contact = new OpenApiContact{ Name = "Los Testers MoviGestion", Email = "movigestion@testers.com" },
-            License = new OpenApiLicense { Name = "Apache 2.0", Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")},
-        });
+        Title = "LosTesters.MoviGestion.API",
+        Version = "v1",
+        Description = "Los Testers MoviGestion Platform API",
+        TermsOfService = new Uri("https://testers-movigestion.com/tos"),
+        Contact = new OpenApiContact { Name = "Los Testers MoviGestion", Email = "movigestion@testers.com" },
+        License = new OpenApiLicense { Name = "Apache 2.0", Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html") },
     });
+});
 
 // Add CORS Policy
 builder.Services.AddCors(options =>
@@ -87,48 +81,36 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader());
 });
 
+// Dependency Injection
 
-
-
-
-// Configure Dependency Injection
-
-// Shared Bounded Context Injection Configuration
+// Shared
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-// Vehicles Bounded Context Injection Configuration
+// Vehicles
 builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
 builder.Services.AddScoped<IVehicleCommandService, VehicleCommandService>();
 builder.Services.AddScoped<IVehicleQueryService, VehicleQueryService>();
 
-// Reports Bounded Context Injection Configuration
+// Reports
 builder.Services.AddScoped<IReportRepository, ReportRepository>();
 builder.Services.AddScoped<IReportCommandService, ReportCommandService>();
 builder.Services.AddScoped<IReportQueryService, ReportQueryService>();
 
-// Shipments Bounded Context Injection Configuration
+// Shipments
 builder.Services.AddScoped<IShipmentRepository, ShipmentRepository>();
 builder.Services.AddScoped<IShipmentCommandService, ShipmentCommandService>();
 builder.Services.AddScoped<IShipmentQueryService, ShipmentQueryService>();
 
-// Profiles Bounded Context Injection Configuration
+// Profiles
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
 builder.Services.AddScoped<IProfileCommandService, ProfileCommandService>();
 builder.Services.AddScoped<IProfileQueryService, ProfileQueryService>();
 
-
-
 var app = builder.Build();
 
-// Verify Database Objects are Created
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
-}
+// ?? Removed EnsureCreated block because RDS can't create database from app directly
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsProduction())
 {
     app.UseSwagger();
